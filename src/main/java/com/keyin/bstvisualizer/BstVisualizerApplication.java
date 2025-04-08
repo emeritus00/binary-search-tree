200b2ba8ce5af4e2b2977f316a161e8268bbcaf4
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2025-04-08 15:30:12 -0230

    Initial commit

---
+++ src/main/java/com/keyin/bstvisualizer/BstVisualizerApplication.java
@@ -0,0 +1,11 @@
+package com.keyin.bstvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstVisualizerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BstVisualizerApplication.class, args);
+    }
+}
